d019495283ff77cf1d146880b80436737ba8db5b
Lam4ek <d.i.iljyusha@student.khai.edu>
2024-07-25 18:40:11 +0200

    memorization changed

---
+++ client/src/components/WorkSpace/StudyPrograms/Memorization/Card.tsx
@@ -0,0 +1,115 @@
+import { FC, useState } from "react";
+import styles from "./Memorization.module.css";
+import { IoMdCheckmark, IoMdClose } from "react-icons/io";
+import { TermData } from "../../../../types/types";
+
+interface Cardprops {
+  step: TermData;
+  correctAnswers: number;
+  setCorrectAnswers: (value: number) => void;
+  setIsFinished: (value: boolean) => void;
+  isLastStep: boolean;
+  next: () => void;
+}
+
+const Card: FC<Cardprops> = ({
+  step,
+  correctAnswers,
+  setCorrectAnswers,
+  setIsFinished,
+  isLastStep,
+  next,
+}) => {
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
+  const [answer, setAnswer] = useState("");
+  const [isAnswered, setIsAnswered] = useState(false);
+
+  const handleAnswer = () => {
+    const inputElement = document.querySelector('input[type="text"]');
+    setIsAnswered(true);
+    if (step.definition.toLowerCase() === answer.toLowerCase()) {
+      if (inputElement) {
+        inputElement.classList.add(styles.correct);
+        setIsAnswerCorrect(true);
+        setCorrectAnswers(correctAnswers + 1);
+      }
+    } else {
+      inputElement?.classList.add(styles.wrong);
+      setIsAnswerCorrect(false);
+    }
+    setTimeout(() => {
+      setAnswer("");
+      inputElement?.classList.remove(styles.correct, styles.wrong);
+      setIsAnswerCorrect(null);
+      setIsAnswered(false);
+      if (isLastStep) {
+        setIsFinished(true);
+      } else {
+        next();
+      }
+    }, 1500);
+  };
+
+  return (
+    <>
+      <div className={styles.termWrapper}>
+        <span>Term</span>
+        <div className={styles.textContainer}>
+          <p className={styles.term}>{step.term}</p>
+          <button style={{ display: "none" }}>Show hint</button>
+        </div>
+      </div>
+
+      <div style={{ marginTop: "15px" }}>
+        <span>Your answer</span>
+
+        <div className={styles.inputWrapper}>
+          <IoMdCheckmark
+            style={{
+              display: `${
+                isAnswerCorrect !== null && isAnswerCorrect === true
+                  ? "block"
+                  : "none"
+              }`,
+            }}
+            className={isAnswerCorrect ? styles.mark : ""}
+            color='#94edcf'
+          />
+          <IoMdClose
+            style={{
+              display: `${
+                isAnswerCorrect !== null && isAnswerCorrect === false
+                  ? "block"
+                  : "none"
+              }`,
+            }}
+            className={!isAnswerCorrect ? styles.mark : ""}
+            color='#fe7773'
+          />
+
+          <input
+            className={styles.answerEntering}
+            placeholder='Enter an answer'
+            value={answer}
+            disabled={isAnswered}
+            type='text'
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+        </div>
+
+        <div className={styles.buttons}>
+          <button
+            onClick={handleAnswer}
+            disabled={isAnswered}
+            className={styles.answerButton}
+          >
+            Answer
+          </button>
+          <button>Don't know?</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- client/src/components/WorkSpace/StudyPrograms/Memorization/index.tsx
+++ client/src/components/WorkSpace/StudyPrograms/Memorization/index.tsx
@@ -1,18 +1,18 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Memorization.module.css";
 import { useMultiCards } from "../../../../Hooks/useMultiCards";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../../../Hooks";
 import { FolderData, ModuleData } from "../../../../types/types";
-import { IoMdCheckmark, IoMdClose } from "react-icons/io";
+import Progressbar from "../../../../ui/Progressbar";
+
 import NotFoundPage from "../../NotFoundPage";
+import Card from "./Card";
 
 function Memorization() {
   const [isLoading, setIsLoading] = useState(true);
   const [isFinished, setIsFinished] = useState(false);
-  const [answer, setAnswer] = useState("");
   const [correctAnswers, setCorrectAnswers] = useState(0);
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
 
   const { moduleId, folderId } = useParams<{
     folderId: string;
@@ -36,34 +36,9 @@ function Memorization() {
       setIsLoading(true);
     }
   }, [module, folder]);
-  const { currentStepIndex, step, steps, isLastStep, next } = useMultiCards(
+  const { step, steps, isLastStep, next } = useMultiCards(
     isLoading ? [] : module.terms
   );
-  console.log(steps.length);
-
-  const handleAnswer = () => {
-    const inputElement = document.querySelector('input[type="text"]');
-    if (step.definition.toLowerCase() === answer.toLowerCase()) {
-      if (inputElement) {
-        inputElement.classList.add(styles.correct);
-        setIsAnswerCorrect(true);
-        setCorrectAnswers(correctAnswers + 1);
-      }
-    } else {
-      inputElement?.classList.add(styles.wrong);
-      setIsAnswerCorrect(false);
-    }
-    setTimeout(() => {
-      setAnswer("");
-      inputElement?.classList.remove(styles.correct, styles.wrong);
-      setIsAnswerCorrect(null);
-      if (isLastStep) {
-        setIsFinished(true);
-      } else {
-        next();
-      }
-    }, 2000);
-  };
 
   if (!module && !folder) {
     return <NotFoundPage />;
@@ -71,79 +46,22 @@ function Memorization() {
   return (
     <>
       {!isLoading && step ? (
-        <div className={styles.card}>
+        <>
           {!isFinished ? (
-            <>
-              <div className={styles.termWrapper}>
-                <span>Term</span>
-                <div className={styles.textContainer}>
-                  <p className={styles.term}>{step.term}</p>
-                  <button style={{ display: "none" }}>Show hint</button>
-                </div>
-              </div>
-
-              <div style={{ marginTop: "15px" }}>
-                <span>Your answer</span>
-
-                <div className={styles.inputWrapper}>
-                  <IoMdCheckmark
-                    style={{
-                      display: `${
-                        isAnswerCorrect !== null && isAnswerCorrect === true
-                          ? "block"
-                          : "none"
-                      }`,
-                    }}
-                    className={isAnswerCorrect ? styles.mark : ""}
-                    color='#94edcf'
-                  />
-                  <IoMdClose
-                    style={{
-                      display: `${
-                        isAnswerCorrect !== null && isAnswerCorrect === false
-                          ? "block"
-                          : "none"
-                      }`,
-                    }}
-                    className={!isAnswerCorrect ? styles.mark : ""}
-                    color='#fe7773'
-                  />
-
-                  <input
-                    className={styles.answerEntering}
-                    placeholder='Enter an answer'
-                    value={answer}
-                    type='text'
-                    onChange={(e) => setAnswer(e.target.value)}
-                  />
-                </div>
-
-                <div className={styles.buttons}>
-                  <button
-                    onClick={handleAnswer}
-                    className={styles.answerButton}
-                  >
-                    Answer
-                  </button>
-                  <button>Don't know?</button>
-                </div>
-              </div>
-            </>
-          ) : (
-            <div className={styles.results}>
-              <div>
-                <span style={{ color: "#94edcf" }}>Correct</span>
-                <span style={{ color: "#fe7773" }}>Incorrect</span>
-              </div>
-              <div>
-                <span style={{ color: "#94edcf" }}>{correctAnswers}</span>
-                <span style={{ color: "#fe7773" }}>
-                  {steps.length - correctAnswers}
-                </span>
-              </div>
+            <div className={styles.card}>
+              <Card
+                step={step}
+                correctAnswers={correctAnswers}
+                setCorrectAnswers={setCorrectAnswers}
+                setIsFinished={setIsFinished}
+                isLastStep={isLastStep}
+                next={next}
+              />
             </div>
+          ) : (
+            <Progressbar score={correctAnswers} totalQuestions={steps.length} />
           )}
-        </div>
+        </>
       ) : (
         "Loading"
       )}
